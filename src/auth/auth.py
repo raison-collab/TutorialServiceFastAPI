3e2478297a1b06dca8e89996d642ae1bbae20438
raisonCollab <danil.zinovev.16@mail.ru>
2024-03-06 11:56:02 +0300

    add auth backend

---
+++ src/auth/auth.py
@@ -0,0 +1,20 @@
+from fastapi_users.authentication import CookieTransport, AuthenticationBackend
+from fastapi_users.authentication import JWTStrategy
+
+from config import TOKEN_SECRET
+
+cookie_transport = CookieTransport(
+    cookie_name='TutoringServiceFastAPIAuth',
+    cookie_max_age=3600  # токен выдается на час
+)
+
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=TOKEN_SECRET, lifetime_seconds=3600)
+
+
+auth_backend = AuthenticationBackend(
+    name="jwt",
+    transport=cookie_transport,
+    get_strategy=get_jwt_strategy,
+)
