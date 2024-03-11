24034c7d023b680d9636ee965a67cf6ea7b6c00d
raisonCollab <danil.zinovev.16@mail.ru>
2024-03-12 00:31:04 +0300

    fix some fields

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -3,8 +3,14 @@ from fastapi_users import schemas
 
 class UserRead(schemas.BaseUser[int]):
     id: int
-    email: str
+    first_name: str
+    second_name: str
+    last_name: str
+    card_number: str
 
 
 class UserCreate(schemas.BaseUserCreate):
-    email: str
+    first_name: str
+    second_name: str
+    last_name: str
+    card_number: str
