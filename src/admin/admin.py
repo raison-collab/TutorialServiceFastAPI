0ca8a9ba8e9e9666409f1e440563ba302501dba4
raisonCollab <danil.zinovev.16@mail.ru>
2024-03-29 12:16:40 +0300

    add some models

--- src/admin/admin.py
+++ src/admin/admin.py
@@ -1,6 +1,7 @@
 import sqladmin
 
 from src.auth.database import Role, User
+from src.main_service.models import SubjectModel, ServiceModel, OrderModel, StatusModel
 
 
 class RoleAdmin(sqladmin.ModelView, model=Role):
@@ -9,3 +10,19 @@ class RoleAdmin(sqladmin.ModelView, model=Role):
 
 class UserAdmin(sqladmin.ModelView, model=User):
     column_list = [User.id, User.email, User.role_id, User.first_name, User.last_name]
+
+
+class SubjectAdmin(sqladmin.ModelView, model=SubjectModel):
+    column_list = [SubjectModel.id, SubjectModel.name]
+
+
+class ServiceAdmin(sqladmin.ModelView, model=ServiceModel):
+    column_list = [ServiceModel.id, ServiceModel.user_id, ServiceModel.subject_id, ServiceModel.amount]
+
+
+class OrderAdmin(sqladmin.ModelView, model=OrderModel):
+    column_list = [OrderModel.id, OrderModel.service_id, OrderModel.user_id, OrderModel.status_id]
+
+
+class StatusAdmin(sqladmin.ModelView, model=StatusModel):
+    column_list = [StatusModel.id, StatusModel.name]
--- src/main_service/models.py
+++ src/main_service/models.py
@@ -1,12 +1,37 @@
-from pydantic import BaseModel
-from sqlalchemy import MetaData, Table, Column, Integer
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Text
+from sqlalchemy.orm import DeclarativeBase
 
-metadata = MetaData()
+from src.auth.database import User
 
 
-test_model = Table(
-    'test_model',
-    metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True)
-)
+class Base(DeclarativeBase):
+    pass
 
+
+class SubjectModel(Base):
+    __tablename__ = "subject"
+    id: int = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    name: str = Column(String(length=80), nullable=False)
+
+
+class ServiceModel(Base):
+    __tablename__ = "service"
+    id: int = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    subject_id: int = Column(Integer, ForeignKey("subject.id"))
+    user_id: User = Column(Integer, ForeignKey(User.id))
+    amount: float = Column(Float, nullable=False)
+    info: str = Column(Text, nullable=False)
+
+
+class OrderModel(Base):
+    __tablename__ = "order"
+    id: int = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    service_id: int = Column(Integer, ForeignKey("service.id"))
+    user_id: User = Column(Integer, ForeignKey(User.id))
+    status_id: int = Column(Integer, ForeignKey("status.id"), nullable=False)
+
+
+class StatusModel(Base):
+    __tablename__ = "status"
+    id: int = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    name: str = Column(String(length=85), nullable=False)
