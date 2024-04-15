aa4619d5a6069954f279c10ba5c64a117b1b6eb4
Zhenya <zhenyaandtema@gmail.com>
2024-04-15 10:33:45 +0300

    add router

--- src/main_service/routers.py
+++ src/main_service/routers.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends
 from loader import fastapi_users
 from src.auth.database import User
 from .db_service import DBService
-from .schemas import SubjectSchema
+from .schemas import SubjectSchema, ServiceSchema, StatusSchema, OrderSchema
 
 router = APIRouter(
     prefix='/api',
@@ -48,7 +48,97 @@ async def delete_subject(subject_id: int):
 
     return {"id": res}
 
-@router.post()
+@router.post('/service')
+async def create_service(data: ServiceSchema):
+    res = await db_service.create_service(data.dict())
+
+    return {'id':res }
+
+@router.get('/services')
+async def get_services():
+    res = await db_service.get_services()
+
+    return {'data': res}
+
+@router.get('/service/{service_id}')
+async def get_service_by_id(service_id: int):
+    res = await db_service.get_service_by_id(service_id)
+
+    return res
+
+@router.put('/service/{service_id}')
+async def update_service(service_id: int, data: ServiceSchema):
+    res = await db_service.update_service(service_id, data.dict())
+
+    return {'id': res}
+
+@router.delete('/service/{service_id}')
+async def delete_service(service_id: int):
+    res = await db_service.delete_service(service_id)
+
+    return {'id': res}
+
+
+@router.post('/status')
+async def create_status(data: StatusSchema):
+    res = await db_service.create_status(data.dict())
+
+    return {'id': res}
+
+@router.get('/statuses')
+async def get_statuses():
+    res = await db_service.get_statuses()
+
+    return {'data': res}
+
+@router.get('/status/{status_id}')
+async def get_status_by_id(status_id: int):
+    res = await db_service.get_status_by_id(status_id)
+
+    return res
+
+@router.put('/status/{status_id}')
+async def update_status(status_id: int, data: StatusSchema):
+    res = await db_service.update_status(status_id, data.dict())
+
+    return {'id': res}
+
+@router.delete('/status/{status_id}')
+async def delete_status(status_id: int):
+    res = await db_service.delete_status(status_id)
+
+    return {'id': res}
+
+@router.post('/order')
+async def create_order(data: OrderSchema):
+    res = await db_service.create_service(data.dict())
+
+    return {'id': res}
+
+@router.get('/orders')
+async def get_orders():
+    res = await db_service.get_orders()
+
+    return {'data': res}
+
+@router.get('/order/{order_id}')
+async def get_status_by_id(order_id: int):
+    res = await db_service.get_status_by_id(order_id)
+
+    return {'id': res}
+
+@router.put('/order/{order_id}')
+async def update_order(order_id: int, data: OrderSchema):
+    res = await db_service.update_order(order_id,data.dict())
+
+    return {'id': res}
+
+@router.delete('/order/{order_id}')
+async def delete_order(order_id: int):
+    res = await db_service.delete_order(order_id)
+
+    return {'id': res}
+
 
 
 @router.get("/")
--- src/main_service/schemas.py
+++ src/main_service/schemas.py
@@ -3,3 +3,12 @@ from pydantic import BaseModel
 
 class SubjectSchema(BaseModel):
     name: str
+
+class ServiceSchema(BaseModel):
+    name: str
+
+class StatusSchema(BaseModel):
+    name: str
+
+class OrderSchema(BaseModel):
+    name: str
